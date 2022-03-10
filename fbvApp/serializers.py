5edd6ff84f7cf2993ea224b60c5fc33291f6bc50
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-10 08:05:09 +0530

    created serializers

---
+++ fbvApp/serializers.py
@@ -0,0 +1,9 @@
+from attr import fields
+from rest_framework import serializers
+from fbvApp.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'exam_score']
