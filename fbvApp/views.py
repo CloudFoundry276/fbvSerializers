cbd4419929096d1037734e6ca297195a6d9cd9a5
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-10 08:29:08 +0530

    created student createion serializer view

--- fbvApp/views.py
+++ fbvApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from fbvApp.models import Student
 from fbvApp.serializers import StudentSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -11,3 +12,9 @@ def student_list(request):
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
