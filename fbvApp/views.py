335f318e7cfcffd2335e7ca26cad1a5b12a8ae8c
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-10 08:13:19 +0530

    created student list serializer view

--- fbvApp/views.py
+++ fbvApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from fbvApp.models import Student
+from fbvApp.serializers import StudentSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+def student_list(request):
+
+    if request.method == "GET":
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
